1b6ebed9d3905be4931c356440eab379cf18b1dd
MA-Moran <118410881+MA-Moran@users.noreply.github.com>
2023-05-21 12:34:54 -0600

    Update Ejer11.js

--- Ejer11.js
+++ Ejer11.js
@@ -1,3 +1,6 @@
+// Si la puntuación es mayor o igual a 60 puntos, la salida se "pasa". Si la puntuación es inferior a 60, el resultado es "no calificado".
+//
+
 var score = 75;         
             
     switch(true){
